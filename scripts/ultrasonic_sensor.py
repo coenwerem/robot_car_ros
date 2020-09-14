c037b04c8c9063686816c2b51e54d6067731fb24
intellimath1 <clintonenwerem@gmail.com>
2020-09-14 09:04:09 +0100

    Modified ultrasonic_sensor.py

--- scripts/ultrasonic_sensor.py
+++ scripts/ultrasonic_sensor.py
@@ -17,39 +17,41 @@ sensorDetails.ECHO = 23
 GPIO.setup(sensorDetails.TRIG, GPIO.OUT)
 GPIO.setup(sensorDetails.ECHO, GPIO.IN)
 
+GPIO.setwarnings(False)
 
 def calcDistance():
 
 	while not rospy.is_shutdown():
 		pub = rospy.Publisher('distance', ultRangerData, queue_size=10)
-        rospy.init_node('sensor_node', anonymous=True)
+		rospy.init_node('sensor_node', anonymous=True)
 		rate = rospy.Rate(2)
 
-        GPIO.output(sensorDetails.TRIG, True)
-        time.sleep(0.00001)
-        GPIO.output(sensorDetails.TRIG, False)
+		GPIO.output(sensorDetails.TRIG, True)
+		time.sleep(0.00001)
+		GPIO.output(sensorDetails.TRIG, False)
 
-        pulse_start = time.time()
+		pulse_start = time.time()
 
-        while GPIO.input(sensorDetails.ECHO) == 0:
-            pulse_start = time.time()
+        	while GPIO.input(sensorDetails.ECHO) == 0:
+            		pulse_start = time.time()
 
-        while GPIO.input(sensorDetails.ECHO) == 1:
-            pulse_end = time.time()
+        	while GPIO.input(sensorDetails.ECHO) == 1:
+            		pulse_end = time.time()
 
-        pulse_duration = pulse_end - pulse_start
-        dist = pulse_duration * 17150
-        sensorDetails.distance = dist
-        success_str = "Distance recorded and published: %f" % dist
-        rospy.loginfo(success_str)
-        pub.publish(sensorDetails)
-        rate.sleep()
+		pulse_duration = pulse_end - pulse_start
+		dist = pulse_duration * 17150
+		sensorDetails.distance = dist
+		success_str = "Distance recorded and published: %f" % dist
+		#rospy.loginfo(success_str)
+		pub.publish(sensorDetails)
+		rate.sleep()
 
 
 if __name__ == "__main__":
 
 	try:
 		calcDistance()
+		GPIO.cleanup()
 
 	except rospy.ROSInterruptException:
 
